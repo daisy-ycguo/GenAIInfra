c63fa20400bcf03657e2bc05cbc0e64376b2ae78
Yingchun Guo <yingchun.guo@intel.com>
2024-07-01 08:58:29 +0800

    revise image build script
    Signed-off-by: Yingchun Guo <yingchun.guo@intel.com>

--- scripts/ci/build-push.sh
+++ scripts/ci/build-push.sh
@@ -4,6 +4,13 @@
 
 export OPEA_IMAGE_REPO=100.83.111.251:5000
 
+function docker_build() {
+  name=$1
+  dockerfile=$2
+  docker build -t $OPEA_IMAGE_REPO/opea/$1:latest -f $2 .
+  docker push $OPEA_IMAGE_REPO/$1:latest
+  docker rmi $OPEA_IMAGE_REPO/$1:latest
+}
 function build_compos() {
   if [ -d "GenAIComps" ]; then
               rm -rf GenAIComps
@@ -11,23 +18,29 @@ function build_compos() {
   git clone https://github.com/opea-project/GenAIComps.git
   cd GenAIComps
   #chatqna
-  docker build -t $OPEA_IMAGE_REPO/opea/embedding-tei:latest -f comps/embeddings/langchain/docker/Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/embedding-tei:latest
+  docker_build embedding-tei comps/embeddings/langchain/docker/Dockerfile
+  #docker build -t $OPEA_IMAGE_REPO/opea/embedding-tei:latest -f comps/embeddings/langchain/docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/embedding-tei:latest
 
-  docker build -t $OPEA_IMAGE_REPO/opea/retriever-redis:latest -f comps/retrievers/langchain/redis/docker/Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/retriever-redis:latest
+  docker_build retriever-redis comps/retrievers/langchain/redis/docker/Dockerfile
+  #docker build -t $OPEA_IMAGE_REPO/opea/retriever-redis:latest -f comps/retrievers/langchain/redis/docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/retriever-redis:latest
 
-  docker build -t $OPEA_IMAGE_REPO/opea/reranking-tei:latest -f comps/reranks/langchain/docker/Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/reranking-tei:latest
+  docker_build reranking-tei comps/reranks/langchain/docker/Dockerfile
+  #docker build -t $OPEA_IMAGE_REPO/opea/reranking-tei:latest -f comps/reranks/langchain/docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/reranking-tei:latest
 
-  docker build -t $OPEA_IMAGE_REPO/opea/llm-tgi:latest -f comps/llms/text-generation/tgi/Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/llm-tgi:latest
+  docker_build llm-tgi comps/llms/text-generation/tgi/Dockerfile
+  #docker build -t $OPEA_IMAGE_REPO/opea/llm-tgi:latest -f comps/llms/text-generation/tgi/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/llm-tgi:latest
 
-  docker build -t $OPEA_IMAGE_REPO/opea/dataprep-redis:latest -f comps/dataprep/redis/langchain/docker/Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/dataprep-redis:latest
-
-  docker build -t $OPEA_IMAGE_REPO/opea/llm-docsum-tgi:latest -f comps/llms/summarization/tgi/Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/llm-docsum-tgi:latest
+  docker_build dataprep-redis comps/dataprep/redis/langchain/docker/Dockerfile
+  #docker build -t $OPEA_IMAGE_REPO/opea/dataprep-redis:latest -f comps/dataprep/redis/langchain/docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/dataprep-redis:latest
+  
+  docker_build llm-docsum-tgi comps/llms/summarization/tgi/Dockerfile
+  #docker build -t $OPEA_IMAGE_REPO/opea/llm-docsum-tgi:latest -f comps/llms/summarization/tgi/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/llm-docsum-tgi:latest
 
   cd ..
 }
@@ -35,8 +48,9 @@ function build_compos() {
 function build_external() {
   git clone https://github.com/huggingface/tei-gaudi
   cd tei-gaudi/
-  docker build --no-cache -f Dockerfile-hpu -t $OPEA_IMAGE_REPO/opea/tei-gaudi:latest .
-  docker push $OPEA_IMAGE_REPO/opea/tei-gaudi:latest
+  docker_build tei-gaudi Dockerfile-hpu
+  #docker build --no-cache -f Dockerfile-hpu -t $OPEA_IMAGE_REPO/opea/tei-gaudi:latest .
+  #docker push $OPEA_IMAGE_REPO/opea/tei-gaudi:latest
   cd ..
 }
 
@@ -57,18 +71,39 @@ function build_mega() {
   fi
   git clone https://github.com/opea-project/GenAIExamples.git
   cd GenAIExamples/ChatQnA/docker
-  docker build --no-cache -t $OPEA_IMAGE_REPO/opea/chatqna:latest -f Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/chatqna:latest
-  cd ../../CodeGen/docker
-  docker build --no-cache -t $OPEA_IMAGE_REPO/opea/codegen:latest -f Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/codegen:latest
-  cd ../../CodeTrans/docker
-  docker build --no-cache -t $OPEA_IMAGE_REPO/opea/codetrans:latest -f Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/codetrans:latest
-  cd ../../DocSum/docker
-  docker build --no-cache -t $OPEA_IMAGE_REPO/opea/docsum:latest -f Dockerfile .
-  docker push $OPEA_IMAGE_REPO/opea/docsum:latest
-  cd ../../
+  docker_build chatqna Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/chatqna:latest -f Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/chatqna:latest
+  #docker rmi $OPEA_IMAGE_REPO/opea/chatqna:latest
+  cd ui
+  docker_build chatqna-ui docker/Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/chatqna-ui:latest -f docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/chatqna-ui:latest
+  cd ../../../CodeGen/docker
+  docker_build codegen Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/codegen:latest -f Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/codegen:latest
+  cd ui
+  docker_build codegen-ui docker/Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/codegen-ui:latest -f docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/codegen-ui:latest
+  cd ../../../CodeTrans/docker
+  docker_build codetrans Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/codetrans:latest -f Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/codetrans:latest
+  cd ui
+  docker_build codetrans-ui docker/Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/codetrans-ui:latest -f docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/codetrans-ui:latest
+  cd ../../../DocSum/docker
+  docker_build docsum Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/docsum:latest -f Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/docsum:latest
+  cd ui
+  docker_build docsum-ui docker/Dockerfile
+  #docker build --no-cache -t $OPEA_IMAGE_REPO/opea/docsum-ui:latest -f docker/Dockerfile .
+  #docker push $OPEA_IMAGE_REPO/opea/docsum-ui:latest
+  cd ../../../
 }
 
 if [ $# -eq 0 ]; then
